33d2a3f60a9130f2bf9d23067b61a5357fffff3a
SAMIA1008 <100969272+SAMIA1008@users.noreply.github.com>
2022-03-17 02:27:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["copywriter", "Marketer", "And Web Developper"],
+        strings: ["Copywriter", "Marketer", "And Web Developper"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["copywriter", "Marketer", "And Web Developper"],
+        strings: ["Copywriter", "Marketer", "And Web Developper"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
